1922e8b38a1ab907f9a1762f4dd76cde825e69b8
davibaweja <davibaweja@gmail.com>
2023-07-15 16:18:08 +0100

    added update route

--- auth/Auth.js
+++ auth/Auth.js
@@ -52,3 +52,36 @@ exports.login = async (req, res, next) => {
     });
   }
 };
+
+exports.update = async (req, res, next) => {
+  const { role, id } = req.body;
+  // Verifying if role and id is presnt
+  if (role && id) {
+    if (role === "admin") {
+      await User.findById(id)
+        .then((user) => {
+          if (user.role !== "admin") {
+            user.role = role;
+            user.save((err) => {
+              if (err) {
+                res
+                  .status("400")
+                  .json({ message: "An error occurred", error: err.message });
+                process.exit(1);
+              }
+            });
+          }
+          res.status(201).json({ message: "Update successful", user });
+        })
+        .catch((error) => {
+          res
+            .status(400)
+            .json({ message: "An error occurred", error: error.message });
+        });
+    } else {
+      res.status(400).json({ message: "User is already an Admin" });
+    }
+  } else {
+    res.status(400).json({ message: "Role or Id not present" });
+  }
+};
