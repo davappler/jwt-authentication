ff49a934e3eec6980e57dfbb8c8cd24adff6d8a9
davibaweja <davibaweja@gmail.com>
2023-07-15 15:46:44 +0100

    created the register end point

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDB = require("./db");
+
+//Connecting the Database
+connectDB();
+
+const app = express();
+app.use(express.json());
+const PORT = 5001;
+
+app.use("/api/auth", require("./auth/Route"));
+
+const server = app.listen(PORT, () =>
+  console.log(`Server Connected to port ${PORT}`)
+);
+// Handling Error
+process.on("unhandledRejection", (err) => {
+  console.log(`An error occurred: ${err.message}`);
+  server.close(() => process.exit(1));
+});
