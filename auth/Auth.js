535eb678af00c469d166c2176b8d766ed6215ca6
davibaweja <davibaweja@gmail.com>
2023-07-15 16:25:02 +0100

    delete function

--- auth/Auth.js
+++ auth/Auth.js
@@ -85,3 +85,17 @@ exports.update = async (req, res, next) => {
     res.status(400).json({ message: "Role or Id not present" });
   }
 };
+
+exports.deleteUser = async (req, res, next) => {
+  const { id } = req.body;
+  await User.findById(id)
+    .then((user) => user.deleteOne())
+    .then((user) =>
+      res.status(201).json({ message: "User successfully deleted", user })
+    )
+    .catch((error) =>
+      res
+        .status(400)
+        .json({ message: "An error occurred", error: error.message })
+    );
+};
--- auth/Route.js
+++ auth/Route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, update } = require("./Auth");
+const { register, login, update, deleteUser } = require("./Auth");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/update").post(update);
+router.route("/deleteUser").post(deleteUser);
 module.exports = router;
