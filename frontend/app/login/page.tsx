88db2274c03c120af3cc69204506c248a28e4dab
davibaweja <davibaweja@gmail.com>
2023-09-12 23:59:50 +0100

    format fix

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 function Login() {
-  function handleLogin(e:React.FormEvent<HTMLFormElement>) {
+  function handleLogin(e: React.FormEvent<HTMLFormElement>) {
     console.log(e);
-    e.preventDefault()
-        const formData = new FormData(e.target as HTMLFormElement)
-    const formDataAsEntries = formData.entries()
-    const formDataAsObject = Object.fromEntries(formDataAsEntries)
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const formDataAsEntries = formData.entries();
+    const formDataAsObject = Object.fromEntries(formDataAsEntries);
 
-    console.log("HAHAH", formDataAsObject)
+    console.log("HAHAH", formDataAsObject);
   }
   return (
     <>
@@ -19,8 +19,19 @@ function Login() {
         className="flex flex-col justify-center items-center w-1/2"
       >
         <h1 className="text-xl font-medium">Login Details</h1>
-        <Input name="username" placeholder="username" className="w-60 mt-2" required/>
-        <Input name="password" placeholder="password" type="password" className="w-60 mt-2" required/>
+        <Input
+          name="username"
+          placeholder="username"
+          className="w-60 mt-2"
+          required
+        />
+        <Input
+          name="password"
+          placeholder="password"
+          type="password"
+          className="w-60 mt-2"
+          required
+        />
         <Button type="submit" className="mt-2">
           Login
         </Button>
